1830c4449e0584a30e6acff5f244845c55dc8259
Eric <congpeng89@gmail.com>
2014-07-14 14:14:28 -0400

    Added accelerate script.
    This script will accelerate the character when it runs on top of an accelerator.

---
+++ Accelerate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accelerate : MonoBehaviour {
+	public float accelerateScale = 10.0f;
+	public PlayerController pc;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			pc = other.gameObject.GetComponent<PlayerController>();
+			pc.movementSpeed = pc.accelerateThreshold;
+		}
+	}
+}
