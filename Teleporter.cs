add1586b9775f1ca8243592899ad741b90d2c616
Eric <congpeng89@gmail.com>
2014-07-15 14:26:50 -0400

    Added teleporter script.
    The character will teleport to another teleporter.

---
+++ Teleporter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			Vector3 targetPosition = (target.transform.position);
+			print (target.transform.position.y);
+			targetPosition.y += 5.3f;
+			other.gameObject.transform.position = targetPosition;
+		}
+	}
+}
