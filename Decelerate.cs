72351e0e99f0ef2a6c97a835bdf2159461808dbd
Eric <congpeng89@gmail.com>
2014-07-16 14:51:14 -0400

    Added decceleration script.
    The player will move at .5x of the optimal speed.

---
+++ Decelerate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Decelerate : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerStay(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.GetComponent<PlayerController>().movementSpeed =  250;
+		}
+	}
+}
