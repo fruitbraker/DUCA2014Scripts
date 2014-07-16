7f0886db0e5926119924cb6aecdb865e007be50a
Eric <congpeng89@gmail.com>
2014-07-16 13:59:21 -0400

    Updated PlayerController.
    Player will now move at an optimal speed and correctly.

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,53 +3,20 @@ using System.Collections;
 using System;
 
 public class PlayerController : MonoBehaviour {
-	public float movementSpeed = 20.0f;
-	public float accelerateThreshold = 50.0f;
-
-	public Vector3 accelVector;
-	public float movementThreshold = 20.0f;
-	public float forwardSpeed = 0.0f;
-	public float leftRightSpeed = 0.0f;
-	public float deccelerateScale = 2f;
-	public float speedThreshold = 10.0f;
-	public bool isAccelerate = false;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	public float movementSpeed;
 	// Update is called once per frame
-	void Update () {
-			forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
-			leftRightSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
+	void FixedUpdate () {
+		float leftRight = Input.GetAxisRaw ("Horizontal");
+		float forward = Input.GetAxisRaw ("Vertical");
 
-			Vector3 movement = new Vector3 (leftRightSpeed, 0, forwardSpeed) + accelVector;
+		Vector3 movement = new Vector3 (leftRight, 0.0f, forward);
 
-			CharacterController cc = GetComponent<CharacterController> ();
-			//rigidbody.velocity = movement;
-			cc.SimpleMove (movement);
+		rigidbody.AddForce (movement * movementSpeed * Time.deltaTime);
 
-			if (movementSpeed > movementThreshold)
-					movementSpeed -= Time.deltaTime * deccelerateScale;
-
-			if (movementSpeed < movementThreshold)
-					movementSpeed += Time.deltaTime * deccelerateScale;
-
-			if (Math.Abs (movementSpeed - movementThreshold) < .025) {
-					movementSpeed = movementThreshold;
-
-			}
-		}
-
-	public void accelerate(Vector3 a) {
-		CharacterController cc = GetComponent<CharacterController> ();
-		cc.slopeLimit = 180;
-		accelVector = a;
 	}
 
 	public void defaultSlope() {
 		CharacterController cc = GetComponent<CharacterController> ();
 		cc.slopeLimit = 45;
-		//accelVector = Vector3(0, 0, 0);
 	}
 }
