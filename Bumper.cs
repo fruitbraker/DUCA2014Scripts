537b668c5338933b90cc4c0e6023250be783e7b8
Eric <congpeng89@gmail.com>
2014-07-17 18:39:17 -0400

    Added Bumper script.
    Character will bounce off in the opposite direction.

---
+++ Bumper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumper : MonoBehaviour {
+
+	void Start() {
+
+	}
+	void Update() {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			if(Input.GetAxisRaw("Vertical") == 1) {
+				other.gameObject.GetComponent<PlayerController>().rigidbody.AddForce(other.gameObject.transform.forward * -1 * 
+			                                         other.gameObject.GetComponent<PlayerController>().movementSpeed * 2f);
+			}
+
+			if(Input.GetAxisRaw("Vertical") == -1) {
+				other.gameObject.GetComponent<PlayerController>().rigidbody.AddForce(other.gameObject.transform.forward * 
+				                                     other.gameObject.GetComponent<PlayerController>().movementSpeed * 2f);
+			}
+		}
+	}
+}
+	
