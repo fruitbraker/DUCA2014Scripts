875587617b20b7dfcd43b3a748366b53e986c5d9
Eric <congpeng89@gmail.com>
2014-07-17 20:46:41 -0400

    Renamed script "Key and Door" to KeyAndDoor.
    Conventions.

---
+++ KeyAndDoor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyandDoor : MonoBehaviour {
+	public GameObject door;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collision other) {
+		Destroy (door);
+		Destroy (other.gameObject);
+	}
+
+}
