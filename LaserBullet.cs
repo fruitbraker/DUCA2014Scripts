3a9d909a05b80303e448331597b7a2c95f4f4cb6
Eric <congpeng89@gmail.com>
2014-07-20 12:48:35 -0400

    Added Laser and Bullet script.
    This is only a draft. This project is now last priority.

---
+++ LaserBullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBullet : MonoBehaviour {
+	public float laserBulletSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.up * Time.deltaTime * laserBulletSpeed);
+
+	}
+}
---
+++ LaserShooter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserShooter : MonoBehaviour {
+	public GameObject laserBullet;
+	public Transform laserBulletSpawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Invoke ("Shoot", 2);
+	}
+
+	void Shoot() {
+		Instantiate (laserBullet, laserBulletSpawn.transform.position, laserBulletSpawn.transform.rotation);
+	}
+
+}
