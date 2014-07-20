c877ccb9b46def33c52f411fed2af7c2b7b12cf7
Eric <congpeng89@gmail.com>
2014-07-20 13:31:52 -0400

    Added SideWaysRollWheel script.
    This will allow a wheel to rotate continuously.

---
+++ SideWaysRollWheel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideWaysRollWheel : MonoBehaviour {
+	public float rotationSpeed;
+	private float rotateCap;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+	}
+}
