a4ffda165732db623969b38d2213ed94b5947648
Eric <congpeng89@gmail.com>
2014-07-20 12:49:39 -0400

    Added FallingPlatform script.
    A platform will now fall at a speed when a player touches certain platform.

---
+++ FallingPlatform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour {
+	private bool shouldFall;
+	public GameObject platform;
+	public float fallSpeed;
+	public float yPos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(shouldFall)
+			transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+		if(transform.position.y < yPos)
+			Destroy(platform);
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Player")
+			shouldFall = true;
+	}
+}
