28b5fde90a20efec1e6015b2b628b3ddcfe8c6ed
Eric <congpeng89@gmail.com>
2014-07-20 12:50:12 -0400

    Added UpDownPlatform script.
    This will allow a playform to go up and down constantly.

---
+++ UpDownPlatform.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpDownPlatform : MonoBehaviour {
+	public float heightCap;
+	public float bottomCap;
+	public float platformSpeed;
+	private bool shouldGoDown;
+	// Use this for initialization
+	void Start () {
+		heightCap += transform.position.y;
+		bottomCap += transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y > heightCap)
+			shouldGoDown = true;
+		if(transform.position.y < bottomCap)
+			shouldGoDown = false;
+		if(!shouldGoDown)
+			transform.Translate(Vector3.up * platformSpeed * Time.deltaTime);
+		if(shouldGoDown)
+			transform.Translate(Vector3.down * platformSpeed * Time.deltaTime);
+	}
+}
