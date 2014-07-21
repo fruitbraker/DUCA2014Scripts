0c0c3941576ecfbfb0cb08b075d1e67b0ecaa4ec
Eric <congpeng89@gmail.com>
2014-07-21 09:20:32 -0400

    Added FallInstantlyRamp script.
    The character will now move normally when it is on a ramp. It will also fall
    normally as well.

---
+++ FallInstantlyRamp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallInstantlyRamp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit(Collider other) {
+		if(other.gameObject.tag == "Player")
+			Physics.gravity = new Vector3(0,-100f,0);
+	}
+}
