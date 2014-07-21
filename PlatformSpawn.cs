72869ffcfbadf1f986db67825600f26bec53924a
Eric <congpeng89@gmail.com>
2014-07-21 09:55:37 -0400

    Renamed variable.

--- PlatformSpawn.cs
+++ PlatformSpawn.cs
@@ -4,20 +4,20 @@ using System.Collections;
 public class PlatformSpawn : MonoBehaviour {
 	public GameObject platform;
 	public Transform platformSpawn;
-	public float heightCap;
+	public float lengthCap;
 	public bool isUp;
 	// Use this for initialization
 	void Start () {
-		heightCap += platform.transform.position.y;
+		lengthCap += platform.transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//This will affect the Y axis
-		if(platform.transform.position.y > heightCap && isUp) {
+		if(platform.transform.position.y > lengthCap && isUp) {
 			platform.transform.position = platformSpawn.transform.position;
 		}
-		if(platform.transform.position.y < heightCap && !isUp) {
+		if(platform.transform.position.y < lengthCap && !isUp) {
 			platform.transform.position = platformSpawn.transform.position;
 		}
 	}
