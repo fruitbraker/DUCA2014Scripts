caa2213f4590f81ec57dfbc29b8e6649a13d6816
Eric <congpeng89@gmail.com>
2014-07-21 00:45:41 -0400

    Added FallInstantly script.
    Character will now fall instantly to his/her doom. This is to resolve an issue
    with gravity (hopefully).

---
+++ FallInstantly.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallInstantly : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			print ("HCEE");
+			other.gameObject.GetComponent<PlayerController>().rigidbody.AddForce(transform.up * 
+			                           other.gameObject.GetComponent<PlayerController>().movementSpeed * -2);
+		}
+	}
+}
