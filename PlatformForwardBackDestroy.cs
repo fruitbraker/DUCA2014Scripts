403b913538960b05daf87983c4d5a695c7f90bdf
Eric <congpeng89@gmail.com>
2014-07-21 09:57:05 -0400

    Added PlatformForwardBack script.
    This is to allow a platform to continuously go in one direction, and go back
    to a respawn point. This will create an illusion the platform is destroyed and
    instantiated again.

---
+++ PlatformForwardBackDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformForwardBackDestroy : MonoBehaviour {
+	public float platformSpeed;
+	public bool isForward;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//This will affect the Z axis
+		if(isForward)
+			transform.Translate (Vector3.forward * Time.deltaTime * platformSpeed);
+		if(!isForward)
+			transform.Translate (Vector3.back * Time.deltaTime * platformSpeed);
+	}
+}
---
+++ PlatformSpawnZAxis.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSpawnZAxis : MonoBehaviour {
+	public GameObject platform;
+	public Transform platformSpawn;
+	public float lengthCap;
+	public bool isForward;
+	// Use this for initialization
+	void Start () {
+		lengthCap += platform.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//This will affect the Z axis
+		if(platform.transform.position.z > lengthCap && isForward) {
+			platform.transform.position = platformSpawn.transform.position;
+		}
+		if(platform.transform.position.z < lengthCap && !isForward) {
+			platform.transform.position = platformSpawn.transform.position;
+		}
+	}
+}
