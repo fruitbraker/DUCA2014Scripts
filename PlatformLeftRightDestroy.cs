b1a37c2dad24c6c2a79ec0263267aa3a2e326ff2
Eric <congpeng89@gmail.com>
2014-07-21 09:57:15 -0400

    Added PlatformLeftRight script.
    This is to allow a platform to continuously go in one direction, and go back
    to a respawn point. This will create an illusion the platform is destroyed and
    instantiated again.

---
+++ PlatformLeftRightDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformLeftRightDestroy : MonoBehaviour {
+	public float platformSpeed;
+	public bool isRight;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//This will affect the X axis
+		if(isRight)
+			transform.Translate (Vector3.right * Time.deltaTime * platformSpeed);
+		if(!isRight)
+			transform.Translate (Vector3.left * Time.deltaTime * platformSpeed);
+	}
+}
---
+++ PlatformSpawnXAxis.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSpawnXAxis : MonoBehaviour {
+	public GameObject platform;
+	public Transform platformSpawn;
+	public float lengthCap;
+	public bool isRight;
+	// Use this for initialization
+	void Start () {
+		lengthCap += platform.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//This will affect the X axis
+		if(platform.transform.position.x > lengthCap && isRight) {
+			platform.transform.position = platformSpawn.transform.position;
+		}
+		if(platform.transform.position.x < lengthCap && !isRight) {
+			platform.transform.position = platformSpawn.transform.position;
+		}
+	}
+}
