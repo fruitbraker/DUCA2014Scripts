91966c9d7022de8094c6a2269e2aab186dd9a5f9
Eric <congpeng89@gmail.com>
2014-07-21 00:12:08 -0400

    Added PlatformUpDown.
    Disregard the "destroy" name. This script will allow a platform to go up
    to a certain height and go back down in an instant, giving an illusion that the platform is "destroyed" and respawned.

---
+++ PlatformSpawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSpawn : MonoBehaviour {
+	public GameObject platform;
+	public Transform platformSpawn;
+	public float heightCap;
+	// Use this for initialization
+	void Start () {
+		heightCap += platform.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(platform.transform.position.y > heightCap) {
+			platform.transform.position = platformSpawn.transform.position;
+		}
+	}
+}
---
+++ PlatformUpDownDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformUpDownDestroy : MonoBehaviour {
+	public float platformSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.up * Time.deltaTime * platformSpeed);
+	}
+}
