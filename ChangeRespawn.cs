4ba0c38bfd41eb3245c58edaed73f0a5a1a4157e
Eric <congpeng89@gmail.com>
2014-07-21 23:33:19 -0400

    Added a ChangeRespawn script.
    The player's respawn point can change once it enters the trigger.

---
+++ ChangeRespawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeRespawn : MonoBehaviour {
+	public GameObject newRespawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Player")
+			other.GetComponent<PlayerController> ().characterRespawn = newRespawn;
+	}
+}
