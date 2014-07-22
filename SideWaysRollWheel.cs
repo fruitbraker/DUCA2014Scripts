2409a72800d8c790338c699074c734db02fb6607
Eric <congpeng89@gmail.com>
2014-07-22 15:01:23 -0400

    Updated SideWaysRollWheel.
    It is to work with all axis.

--- SideWaysRollWheel.cs
+++ SideWaysRollWheel.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class SideWaysRollWheel : MonoBehaviour {
 	public float rotationSpeed;
+	public bool isZ, isY, isX;
+	public bool isReverse;
 	private float rotateCap;
 	// Use this for initialization
 	void Start () {
-
+		if(isReverse)
+			rotationSpeed *= -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+		if(isX)
+			transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+		else if(isY)
+			transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+		else if(isZ)
+			transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
 	}
 }
