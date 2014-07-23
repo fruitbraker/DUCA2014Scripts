34d5a75a261597a523e68de0b1ac9eb04b93f6f0
Eric <congpeng89@gmail.com>
2014-07-22 22:49:25 -0400

    Attempted to implement laser shooting.
    Still broken.

--- LaserBullet.cs
+++ LaserBullet.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class LaserBullet : MonoBehaviour {
 	public float laserBulletSpeed;
+	public bool isLeft;
+
 	// Use this for initialization
 	void Start () {
-	
+		if(isLeft)
+			laserBulletSpeed *= -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.up * Time.deltaTime * laserBulletSpeed);
-
+		transform.Translate (Vector3.right * Time.deltaTime * laserBulletSpeed);
 	}
 }
--- LaserShooter.cs
+++ LaserShooter.cs
@@ -4,19 +4,23 @@ using System.Collections;
 public class LaserShooter : MonoBehaviour {
 	public GameObject laserBullet;
 	public Transform laserBulletSpawn;
+	public float bulletDistance;
+	public bool isRight;
+	public float lengthCap;
 
 	// Use this for initialization
 	void Start () {
-	
+		lengthCap += laserBullet.transform.position.x;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		Invoke ("Shoot", 2);
+		if(laserBullet.transform.position.x > lengthCap && isRight) {
+			laserBullet.transform.position = laserBullet.transform.position;
+		}
+		if(laserBullet.transform.position.x < lengthCap && !isRight) {
+			laserBullet.transform.position = laserBullet.transform.position;
+		}
 	}
-
-	void Shoot() {
-		Instantiate (laserBullet, laserBulletSpawn.transform.position, laserBulletSpawn.transform.rotation);
-	}
-
 }
+	
