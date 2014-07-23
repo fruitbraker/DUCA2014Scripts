bcd6e50171489b5911706946cb2c9cf608e8fee7
Eric <congpeng89@gmail.com>
2014-07-22 22:50:34 -0400

    Updated Accelerate script.
    "Teleporting" is now only applicable to level 5.

--- Accelerate.cs
+++ Accelerate.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Accelerate : MonoBehaviour {
-
+	public bool isLevel5;
 	// Update is called once per frame
 	void Update () {
 		
@@ -10,8 +10,10 @@ public class Accelerate : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Player") {
-			other.gameObject.GetComponent<PlayerController>().rigidbody.velocity = Vector3.zero;
-			other.gameObject.transform.position = transform.position;
+			if(isLevel5){
+				other.gameObject.GetComponent<PlayerController>().rigidbody.velocity = Vector3.zero;
+				other.gameObject.transform.position = transform.position;
+			}
 			other.gameObject.GetComponent<PlayerController>().rigidbody.AddForce(transform.right *
 			                other.gameObject.GetComponent<PlayerController>().movementSpeed * .4f);
 		}
