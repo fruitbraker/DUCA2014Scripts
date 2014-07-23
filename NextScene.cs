647328142bec33175fdfd91d68195a8077329bb6
Eric <congpeng89@gmail.com>
2014-07-23 10:21:54 -0400

    Added NextScene script.
    This script will control the level/scene changes.

---
+++ NextScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextScene : MonoBehaviour {
+	public string sceneName;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			Application.LoadLevel(sceneName);
+		}
+	}
+}
