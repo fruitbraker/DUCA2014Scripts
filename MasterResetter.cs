b72eaea1d6756df7ede0232d267a3d34f6b7dd33
Eric <congpeng89@gmail.com>
2014-07-24 10:13:15 -0400

    Added MasterResetter script.
    Will reset an array of object to its original position when the player dies.

---
+++ MasterResetter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterResetter : MonoBehaviour {
+	public Transform[] gameObjects;
+	private Vector3[] positions;
+	public static bool shouldReset;
+	// Use this for initialization
+	void Start () {
+		shouldReset = false;
+		positions = new Vector3[gameObjects.Length];
+		for(int i=0; i<gameObjects.Length; i++) {
+			positions[i] = gameObjects[i].position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(shouldReset) {
+			for(int i=0; i<gameObjects.Length; i++) {
+				gameObjects[i].position = positions[i];
+			}
+			shouldReset = false;
+		}
+	}
+}
