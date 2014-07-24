16be2224ffdb6e96888f7776b889811497977f11
Eric <congpeng89@gmail.com>
2014-07-24 09:29:12 -0400

    Added Credits script.
    After 16 seconds, the length of the credit, the player will be brought back
    to the main menu ("Opening Scene").

---
+++ CREDITS.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CREDITS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("GOBACK", 16);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GOBACK() {
+		Application.LoadLevel("OpeningScene");
+	}
+}
